a1fa5deb7eb169720991414b4d530b1d0c982a53
midlajc <mail.midlajc@gmail.com>
2022-03-08 14:09:22 +0530

    ++

--- public/js/update_application_status.js
+++ public/js/update_application_status.js
@@ -1,14 +1,14 @@
-let Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
+// let Toast = Swal.mixin({
+//     toast: true,
+//     position: 'top-end',
+//     showConfirmButton: false,
+//     timer: 3000,
+//     timerProgressBar: true,
+//     didOpen: (toast) => {
+//         toast.addEventListener('mouseenter', Swal.stopTimer)
+//         toast.addEventListener('mouseleave', Swal.resumeTimer)
+//     }
+// })
 
 let verifyApplication = () => {
     $.ajax({
